865155a1c5657a92e2d46d89b1c84ca8b87a166c
MariaAPr <prokopenkovamarija@rambler.ru>
2023-01-15 01:38:22 +0400

    hm1401

---
+++ task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+kekDegree(A, B);
+
+void kekDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(a + " в степени " + b + " = " + result);
+}
